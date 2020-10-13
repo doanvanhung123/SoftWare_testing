8707268f072eb048ad516f226ed75c8b4e8d50ec
doanvanhung123 <55818598+doanvanhung123@users.noreply.github.com>
2020-10-13 13:50:24 +0700

    lap 03

--- Test Suites/lab 01.ts
+++ Test Suites/lab 01.ts
@@ -11,4 +11,34 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>1bce3d40-716b-4cb2-880a-8574dda789dd</testSuiteGuid>
+   <testCaseLink>
+      <guid>ba886461-5c24-4263-98ed-0a743d3274f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>467b4f85-adde-4deb-ae59-0239cb2b077e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33764de3-e2e7-4b0c-af42-3b77d9a789ff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>169f71ef-552c-4cb7-8bf5-b4641ff5da38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d6e183f-ffda-4df7-9743-705f9b686b77</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 5</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
