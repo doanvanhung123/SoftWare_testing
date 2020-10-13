095c371690474b9ea3b634b98a44b27774dc946e
doanvanhung123 <55818598+doanvanhung123@users.noreply.github.com>
2020-10-13 15:30:01 +0700

    lap 08

---
+++ Test Suites/lap 08.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>lap 08</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>02b093d4-a95d-4fbd-8f49-a6e040cf8166</testSuiteGuid>
+   <testCaseLink>
+      <guid>4db4fcfb-6f9c-4888-b463-62af6f158940</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 07_lap 8</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>bd42417a-cf3f-40d5-a060-d452c3fa2ba3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/datalap8</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>bd42417a-cf3f-40d5-a060-d452c3fa2ba3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>user</value>
+         <variableId>1a580a3b-f5f2-4997-ae29-e5f8b9e0f3d2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>bd42417a-cf3f-40d5-a060-d452c3fa2ba3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>53ec77a8-7483-47bb-9adb-b4b17a99f761</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
